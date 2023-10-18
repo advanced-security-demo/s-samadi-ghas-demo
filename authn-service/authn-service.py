1fa6675f3c5311d01ed9cb7efa28c5a318088a27
Shadi Samadi <s-samadi@github.com>
2023-10-18 17:21:24 +1100

    Update authn-service.py

--- authn-service/authn-service.py
+++ authn-service/authn-service.py
@@ -54,7 +54,7 @@ def get_user_profile(access_token):
 
 @app.route("/authenticate/<code>")
 def authenticate(code):
-    app.logger.debug("Received authentication request with code: %s", code)
+
     access_info, error = get_access_token(code)
 
     if error:
